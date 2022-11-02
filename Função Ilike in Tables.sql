d26d216b2ccab4c90639beeb3e91b67c2ac45e43
Vinicius Salvador <85971943+ViniciusSalvador0@users.noreply.github.com>
2022-11-02 12:35:19 -0300

    Update Função Ilike in Tables.sql

--- Função Ilike in Tables.sql
+++ Função Ilike in Tables.sql
@@ -1,35 +1,38 @@
-CREATE OR REPLACE FUNCTION PUBLIC.fn_ilike_in_tables (VALOR VARCHAR, SCHEMA VARCHAR) 
-  RETURNS TABLE (
-      coluna VARCHAR,
-      tabela VARCHAR
-  ) AS 
-$$
-DECLARE
-    rQUERY_RESULT RECORD;
-    vCMD VARCHAR;
-BEGIN
-    vCMD := '';
-    
-    FOR rQUERY_RESULT IN (
-    
-         SELECT X.TABLE_NAME,
-                X.COLUMN_NAME, TABLE_SCHEMA
-           FROM INFORMATION_SCHEMA.COLUMNS X
-          WHERE X.TABLE_SCHEMA = SCHEMA
-            AND UPPER(X.DATA_TYPE) IN  ('CHARACTER VARYING','TEXT')
-            AND X.IS_UPDATABLE = 'YES'
+CREATE OR REPLACE FUNCTION PUBLIC.LIKE_IN_TABLES (VALOR VARCHAR, SCH VARCHAR 
+ )
+ RETURNS TABLE
+ (
+ COLUNA VARCHAR,
+ TABELA VARCHAR
+ ) AS
+ $BODY$
+ DECLARE
+    TEMPROW RECORD;
+    CMD VARCHAR;
+ BEGIN
+    CMD := '';
  
-    ) LOOP
+    FOR TEMPROW IN (
     
-         vCMD := vCMD ||  
-                 ' SELECT '|| rQUERY_RESULT.COLUMN_NAME ||'::VARCHAR AS COLUNA, ' || '''' || rQUERY_RESULT.TABLE_NAME || '''' || '::VARCHAR AS TABELA ' || 
-                   ' FROM '|| SCHEMA||'.'|| rQUERY_RESULT.TABLE_NAME || 
-                  ' WHERE '|| rQUERY_RESULT.COLUMN_NAME || '::VARCHAR ILIKE ' || '''' || VALOR || '''' || 
-                  ' UNION SELECT NULL, NULL'; 
+        SELECT X.TABLE_NAME,
+               X.COLUMN_NAME
+          FROM INFORMATION_SCHEMA.COLUMNS X
+         WHERE X.TABLE_SCHEMA = $2
+           AND X.DATA_TYPE IN  ('CHARACTER VARYING','TEXT')
+           AND X.IS_UPDATABLE = 'YES'
                
+    ) LOOP
+         
+         CMD := CMD ||  ' SELECT '|| TEMPROW.COLUMN_NAME ||'::VARCHAR AS COLUNA, '|| '''' || TEMPROW.TABLE_NAME|| '''' ||'::VARCHAR AS TABELA ' || 
+                          ' FROM '||SCH||'.'|| TEMPROW.TABLE_NAME || 
+                         ' WHERE '|| TEMPROW.COLUMN_NAME || '::VARCHAR ILIKE ' || '''' || $1 || '''' || 
+                         ' UNION '; 
+    
     END LOOP;
+         CMD := CMD || ' SELECT NULL, NULL ';
  
-    RETURN QUERY EXECUTE vCMD;   
-END $$ 
-LANGUAGE 'plpgsql' 
-  CALLED ON NULL INPUT;
+    RETURN QUERY EXECUTE CMD;   
+ END;
+ $BODY$
+ LANGUAGE 'plpgsql'
+ CALLED ON NULL INPUT; 
